b49a44691a554bd628c3a3a0e1dcbabe8c6b8a85
VM18042-prn335-2019 <larok20.mm@gmail.com>
2019-08-29 11:50:32 -0600

    base con mapeo de entidades

---
+++ src/test/java/ues/occ/edu/sv/ingenieria/prn335/guia02/controller/ClasificacionBeanTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ues.occ.edu.sv.ingenieria.prn335.guia02.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author mauricio
+ */
+public class ClasificacionBeanTest {
+    
+    public ClasificacionBeanTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
