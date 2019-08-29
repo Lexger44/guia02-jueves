0da5e617939bbbef699ad0eb2a665d13baf3756d
VM18042-prn335-2019 <larok20.mm@gmail.com>
2019-08-29 12:33:58 -0600

    tercer commit con entitymanagerfactory hecho

--- src/main/java/ues/occ/edu/sv/ingenieria/prn335/guia02/controller/ClasificacionBean.java
+++ src/main/java/ues/occ/edu/sv/ingenieria/prn335/guia02/controller/ClasificacionBean.java
@@ -5,10 +5,26 @@
  */
 package ues.occ.edu.sv.ingenieria.prn335.guia02.controller;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import ues.occ.edu.sv.ingenieria.prn335.guia02.jueves.Clasificacion;
+
 /**
  *
  * @author mauricio
  */
 public class ClasificacionBean {
-    
+
+    protected EntityManager em;
+
+    public void crear(Clasificacion r) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("cinePU");
+        this.em = emf.createEntityManager();
+        EntityTransaction tx = this.em.getTransaction();
+
+        tx.commit();
+
+    }
 }
