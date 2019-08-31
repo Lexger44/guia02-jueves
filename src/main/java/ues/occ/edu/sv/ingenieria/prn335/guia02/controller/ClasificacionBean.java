35c7b6da3edf45fd6a8d271b45e32a5506f7aace
VM18042-prn335-2019 <larok20.mm@gmail.com>
2019-08-30 19:42:05 -0600

    terminadas pruebas de unidad funcionales

--- src/main/java/ues/occ/edu/sv/ingenieria/prn335/guia02/controller/ClasificacionBean.java
+++ src/main/java/ues/occ/edu/sv/ingenieria/prn335/guia02/controller/ClasificacionBean.java
@@ -9,6 +9,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.*;
 import ues.occ.edu.sv.ingenieria.prn335.guia02.jueves.Clasificacion;
 
 /**
@@ -18,13 +19,60 @@ import ues.occ.edu.sv.ingenieria.prn335.guia02.jueves.Clasificacion;
 public class ClasificacionBean {
 
     protected EntityManager em;
+    final static EntityManagerFactory EMF = Persistence.createEntityManagerFactory("cinePU");
+
+    public void iniciarEm() {
+        this.em = EMF.createEntityManager();
+    }
+
+    public EntityTransaction getTx() {
+        if (this.em != null) {
+            return this.em.getTransaction();
+        }
+        return null;
+    }
 
     public void crear(Clasificacion r) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("cinePU");
-        this.em = emf.createEntityManager();
-        EntityTransaction tx = this.em.getTransaction();
-
+        EntityTransaction tx = this.getTx();
+        tx.begin();
+        this.em.persist(r);
         tx.commit();
 
+    }
+
+    public void modificar(Clasificacion r) {
+
+        EntityTransaction tx = this.getTx();
+        tx.begin();
+        em.merge(r);
+        tx.commit();
+
+    }
+
+    public String clasificacionPorDescripcion(String clasificacion) {
+        String descripcion = "";
+        String cadena = "";
+        if (clasificacion == "A") {
+            String clasifica = "familiares";
+            String escenas = "comedia";
+            String publico = "todo publico";
+            descripcion = "Clasifica las películas " + clasifica + " y que contengan escenas de " + escenas + " se considera que puede ser vista por " + publico;
+
+            return descripcion;
+        } else if (clasificacion == "B") {
+            String clasifica = "de ficcion";
+            String escenas = "accion";
+            String publico = "mayores de 12 años";
+            descripcion = "Clasifica las películas " + clasifica + " y que contengan escenas de " + escenas + " se considera que puede ser vista por " + publico;
+            return descripcion;
+        } else if (clasificacion == "C") {
+            String clasifica = "de terror";
+            String escenas = "violencia";
+            String publico = "mayores de 15 años";
+            descripcion = "Clasifica las películas " + clasifica + " y que contengan escenas de " + escenas + " se considera que puede ser vista por " + publico;
+            return descripcion;
+        } else {
+            return "Clasificacion no encontrada";
+        }
     }
 }
--- src/test/java/ues/occ/edu/sv/ingenieria/prn335/guia02/controller/ClasificacionBeanTest.java
+++ src/test/java/ues/occ/edu/sv/ingenieria/prn335/guia02/controller/ClasificacionBeanTest.java
@@ -5,42 +5,77 @@
  */
 package ues.occ.edu.sv.ingenieria.prn335.guia02.controller;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ues.occ.edu.sv.ingenieria.prn335.guia02.jueves.Clasificacion;
 
 /**
  *
  * @author mauricio
  */
+@ExtendWith(MockitoExtension.class)
 public class ClasificacionBeanTest {
-    
+
     public ClasificacionBeanTest() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
+
     }
 
     @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void crearTest() throws Exception {
+        System.out.println("crear");
+        EntityManager mockEM = Mockito.mock(EntityManager.class);
+        EntityTransaction mockTX = Mockito.mock(EntityTransaction.class);
+
+        Mockito.when(mockEM.getTransaction()).thenReturn(mockTX);
+        ClasificacionBean cut = new ClasificacionBean();
+        cut.em = mockEM;
+        Clasificacion r = new Clasificacion();
+        r.setClasificacion("A");
+        r.setDescripcion("Familiar");
+        r.setIdClasificacion(1);
+        cut.crear(r);
+        Mockito.verify(mockEM, Mockito.times(1)).persist(Mockito.any());
     }
-    
+
+    @Test
+    public void modificarClasificacionTest() throws Exception {
+        System.out.println("modificarClasificacion");
+        EntityManager mockEM = Mockito.mock(EntityManager.class);
+        EntityTransaction mockTX = Mockito.mock(EntityTransaction.class);
+        Mockito.when(mockEM.getTransaction()).thenReturn(mockTX);
+        Clasificacion r = new Clasificacion();
+        ClasificacionBean cut = new ClasificacionBean();
+        cut.em = mockEM;
+        r.setClasificacion("A");
+        r.setDescripcion("familiares, comedia, todo público");
+        cut.crear(r);
+        System.out.println(r.getClasificacion() + "\n" + r.getDescripcion());
+        r.setClasificacion("B");
+        r.setDescripcion("de ficcion, accion, mayores 12 años");
+        cut.modificar(r);
+        Mockito.verify(mockEM, Mockito.times(1)).persist(Mockito.any());
+        System.out.println(r.getClasificacion() + "\n" + r.getDescripcion());
+//        fail("la hora sad :"v");
+    }
+
+    @Test
+    public void clasificacionPorDescripcion() throws Exception {
+        System.out.println("clasificacionPorDescripcion");
+        ClasificacionBean cut = new ClasificacionBean();
+        String cadena = "alguna descripcion";
+        cadena=cut.clasificacionPorDescripcion("A");
+        assertEquals(cadena, "Clasifica las películas familiares y que contengan escenas de comedia se considera que puede ser vista por todo publico");
+//        fail("esta vaina no funciona");
+
+//        cadena=cut.clasificacionPorDescripcion("B");
+//        assertEquals(cadena, "Clasifica las películas de ficcion y que contengan escenas de accion se considera que puede ser vista por mayores de 12 años");
+//
+//        cadena = cut.clasificacionPorDescripcion("C");
+//        assertEquals(cadena, "Clasifica las películas de terror y que contengan escenas de violencia se considera que puede ser vista por mayores de 15 años");
+    }
 }
